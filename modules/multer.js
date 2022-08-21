0aca8b2f431548aecae0e577e7781e279337eb0b
onpyeong <gsh447114@gmail.com>
2022-08-21 16:07:15 +0900

    add: multer 파일 저장

---
+++ modules/multer.js
@@ -0,0 +1,19 @@
+const multer = require("multer");
+const path = require("path");
+
+const uploadImage = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) { //파일 저장 위치
+      done(null, 'uploads');
+    },
+    filename(req, file, done) {
+      // 파일명 + 현재시간.확장자 = 전체 파일명
+      //const ext = path.extname(file.originalname);
+      //done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+      done(null, 'uploadImage' + path.extname(file.originalname));
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 }, //5MB로 제한
+});
+
+module.exports = { uploadImage }
