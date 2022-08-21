ae9b18058a3d1c490c3f6cacfc136b0c33907bc9
onpyeong <gsh447114@gmail.com>
2022-08-22 03:02:59 +0900

    feat: post api - db에 image path 저장 후 id return

--- routes/upload.js
+++ routes/upload.js
@@ -2,10 +2,11 @@ const express = require('express');
 const uploadMulter = require('../modules/multer');
 const util = require('../modules/util');
 const statusCode = require('../modules/statusCode');
+const Table = require('../models');
 
 const router = express.Router();
 
-router.post('/', uploadMulter.uploadImage.single('image'), (req, res) => {
+router.post('/', uploadMulter.uploadImage.single('image'), async (req, res) => {
 
   if (req.body == undefined) return res.status(statusCode.BAD_REQUEST)
     .send(util.fail
@@ -24,16 +25,32 @@ router.post('/', uploadMulter.uploadImage.single('image'), (req, res) => {
     );
 
   try {
-    //파일을 py로 보내서 처리값 string을 body에 넣음
+    const path = req.file.path;
+    //파일을 py로 보내서 처리값 string을 받음
+    const type = null;
+    let imageId = null;
+    const uploadImage = await Table.Result.create({
+      path: path, type: type
+    }).then((dbRes) => {
+      console.log(dbRes.id);
+      imageId = dbRes.id;
+    });
     res.status(statusCode.OK).send(
       util.success(
         statusCode.OK,
         "image post sucess",
-        "리턴 쓰레기 종류 예정"
+        {imageId : imageId}
+      )
+    )
+  } catch (error) {
+    console.log(error);
+    ErrorCaptureStackTrace(err);
+    res.status(statusCode.DB_ERROR).send(
+      util.fail(
+        statusCode.DB_ERROR,
+        "DB create error"
       )
     );
-  } catch (error) {
-
   }
 
 });
