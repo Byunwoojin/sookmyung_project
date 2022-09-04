ef1b533345d744a6ed197f59b973b4a1d281b0b5
Byunwoojin <sforot9@gmail.com>
2022-09-04 13:01:26 +0900

    fix: cors origin url

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(
 
 app.use(express.json());
 app.use(
-  cors({ origin: ["http://localhost:3000", "https://catch-front.vercel.app/"] })
+  cors({ origin: ["http://localhost:3000", "https://catch-front.vercel.app"] })
 );
 
 app.set("port", process.env.PORT || 3000);
