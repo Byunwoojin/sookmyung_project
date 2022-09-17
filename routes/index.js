f74d6f2816774c33f8ecc960f40611e3d08b49ab
Byunwoojin <sforot9@gmail.com>
2022-09-17 18:14:35 +0900

    delete: CNN

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
 const express = require("express");
 
 const router = express.Router();
-const { spawn } = require("child_process");
 
 // GET / 라우터
 router.get("/", (req, res) => {
-  const analysis = spawn("python", ["mode.py", "카레유", "20"]);
-  console.log(analysis);
-  analysis.stdout.on("data", (result) => {
-    console.log(result.toString());
-    return res.json({
-      data: result.toString(),
-    });
-  });
+  res.send("Hello Express");
 });
 module.exports = router;
