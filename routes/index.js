85da845e28887a6bc34fe487628bae84c4b778c3
Byunwoojin <sforot9@gmail.com>
2022-09-17 18:03:43 +0900

    feat: CNN 테스트

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 
 const router = express.Router();
+const { spawn } = require("child_process");
 
 // GET / 라우터
 router.get("/", (req, res) => {
   res.send("Hello Express");
+  const analysis = spawn("python", ["mode.py", "카레유", "20"]);
+
+  analysis.stdout.on("data", (result) => {
+    console.log(result.toString());
+    return res.json({
+      data: result.toString(),
+    });
+  });
 });
 module.exports = router;
